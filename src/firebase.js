7b1b194d0589c2a08a5d131d18e02cab87d7616d
Abhishek Verma <zabhishek.verma@gmail.com>
2022-07-06 14:04:45 +0530

    Set Up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARZNawhq0owYEbFCLfvqLToc-7gmvwxMo",
+  authDomain: "ecommerce-app-72099.firebaseapp.com",
+  projectId: "ecommerce-app-72099",
+  storageBucket: "ecommerce-app-72099.appspot.com",
+  messagingSenderId: "112265540415",
+  appId: "1:112265540415:web:c45fe9de3c2c6db1fed98a",
+  measurementId: "G-CY34DY2ZMT",
+};
