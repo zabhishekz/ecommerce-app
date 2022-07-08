32b30c0422761b8fbdc8dce7788ccb029dda4e7b
Abhishek Verma <zabhishek.verma@gmail.com>
2022-07-08 15:25:33 +0530

    corrrections

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -27,7 +27,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>🌟</p>
+              <p>⭐</p>
             ))}
         </div>
         <button onClick={removeFromBasket}>Remove from Basket</button>
